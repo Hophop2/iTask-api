ddda7a152d259049f526769f91f225329cdfd9fb
Hophop2 <hubertc72@gmail.com>
2023-11-12 17:56:22 +0100

    url change

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,7 @@ const allowedOrigins = [
   "https://task-manager-eoel.onrender.com",
   "https://i-task-api.vercel.app",
   "https://i-task.vercel.app",
+  "https://i-task-hophop2.vercel.app",
 ];
 
 module.exports = allowedOrigins;
